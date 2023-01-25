9b59139e24e72dec344c47fa7de82f0cfc38d066
LexHeadko <n-odes11rus@yandex.ru>
2023-01-25 19:10:50 +0200

    Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        //task2();
-        //task3();
+        task2();
+        task3();
         //task4();
         //task5();
         //task6();
@@ -19,13 +19,37 @@ public class Main {
             System.out.println(first[i]);
         }
         double [] second = {1.57, 7.654, 9.986};
-        for (int i = 0; i < second.length; i++) {
-            System.out.println(second[i]);
+        for (double v : second) {
+            System.out.println(v);
+            /*for (int i = 0; i < second.length; i++) {
+            System.out.println(second[i]);*/
         }
         int [] third = new int[5];
         for (int i = 0; i < third.length; i++) {
             third[i] = i*3;
             System.out.println(third[i]);
         }
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int [] first = new int[3];
+        for (int i = 0; i < first.length; i++) {
+            first[i] = i+1;
+            System.out.print(first[i] + ", ");
+        }
+        System.out.println();
+        double [] second = {1.57, 7.654, 9.986};
+        for (int i = 0; i < second.length; i++) {
+            System.out.print(second[i] + ", ");
+        }
+        System.out.println();
+        int [] third = new int[5];
+        for (int i = 0; i < third.length; i++) {
+            third[i] = i*3;
+            System.out.print(third[i] + ", ");
+        }
+    }
+    public static void task3() {
+
     }
 }
