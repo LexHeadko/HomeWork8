4dced646c8441649373cc3ce789cfcac9f7ac289
LexHeadko <n-odes11rus@yandex.ru>
2023-01-27 16:02:20 +0200

    Задача 3

--- src/Main.java
+++ src/Main.java
@@ -48,8 +48,34 @@ public class Main {
             third[i] = i*3;
             System.out.print(third[i] + ", ");
         }
+        System.out.println();
     }
     public static void task3() {
-
+        System.out.println("Задача 3");
+        int [] first = new int[3];
+        for (int i = (first.length-1); i >= 0; i--) {
+            first[i] = i+1;
+            System.out.print(first[i]);
+            if (i>0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        double [] second = {1.57, 7.654, 9.986};
+        for (int i = (second.length - 1); i >= 0; i--) {
+            System.out.print(second[i]);
+            if (i>0) {
+                System.out.print(", ");
+            }
+        }
+        System.out.println();
+        int [] third = new int[5];
+        for (int i = third.length-1; i >= 0; i--) {
+            third[i] = i*3;
+            System.out.print(third[i]);
+            if (i>0) {
+                System.out.print(", ");
+            }
+        }
     }
 }
